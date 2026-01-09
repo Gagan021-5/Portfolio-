e7907120130a45cb6c33b2448262feb9a459a17e
Gagan021-5 <gagansingh010221@gmail.com>
2026-01-09 12:40:43 +0530

    updates

--- src/utils/Projects.js
+++ src/utils/Projects.js
@@ -3,6 +3,7 @@ import garc from "../assets/gamearc.png";
 import moviesai from "../assets/moviescam.png";
 import houseprice from "../assets/houseprice.png"
 import nutrition from "../assets/nutrivigil.png";
+import intentyt from "../assets/extension.png"
 const project = [
   {
     url: "https://gamearc-frontend2.onrender.com/",
@@ -43,6 +44,7 @@ const project = [
       "Developed a predictive model using linear regression and feature engineering to forecast house prices.Achieved 81% accuracy by optimizing key features and applying machine learning techniques.",
     skills: ["React", "Tailwind", "Flask", "Scikit-Learn"],
   },
+
   {
     url: "http://nutf.onrender.com/",
     img: nutrition,
@@ -58,6 +60,14 @@ const project = [
     description:
       "A visually accurate and responsive Netflix clone built with vanilla HTML, CSS, and JavaScript. Replicates core UI features like carousels, interactive cards, and mobile-friendly design with a focus on performance.",
     skills: ["HTML", "Tailwind CSS", "JavaScript"],
+  },
+    {
+    url: "https://github.com/Gagan021-5/IntentYoutube",
+    img: intentyt,
+    title: "IntentYoutube",
+    description:
+      "Watch YouTube with intent by controlling recommendations, shorts, and distractions without affecting the video you choose.",
+    skills: ["React", "Chrome Extension APIs"],
   },
 ];
 
